686b5bf74ead47246461fba750a79e697761a688
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2019-12-03 23:37:30 +0530

    Updates

---
+++ precache-manifest.7ff8be175d0ee461ceebca464bf0f216.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f3dc3d87600a3fdba1ec784dff72ba",
+    "url": "/battle/index.html"
+  },
+  {
+    "revision": "ff0588a1f00a318a85f3",
+    "url": "/battle/static/css/main.f9108918.chunk.css"
+  },
+  {
+    "revision": "4393ca48cd702ed88383",
+    "url": "/battle/static/js/2.fcc07511.chunk.js"
+  },
+  {
+    "revision": "ff0588a1f00a318a85f3",
+    "url": "/battle/static/js/main.04ec74ef.chunk.js"
+  },
+  {
+    "revision": "807d8c8222b4451cc8da",
+    "url": "/battle/static/js/runtime-main.0937e3b4.js"
+  }
+]);
